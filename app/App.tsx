bfde9e0baffbd3a3b87c344423932bed42899a8a
Garvit Sharma <161028881+garvit0707@users.noreply.github.com>
2026-04-02 15:35:32 +0530

    expo root setup removed unused code

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import { Text, View } from "react-native";
 
 export class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>App this is the testing code</Text>
+        <Text>App testing code wokring fine in my applicatins</Text>
       </View>
     );
   }
